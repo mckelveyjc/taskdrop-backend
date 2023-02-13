2df035acce94ea124c616ba1f83e4d8944986fa0
Turing <Turing@acad-jbws-10-4-66-93.stedwards.edu>
2023-02-13 14:02:48 -0600

    .

--- updateTask.py
+++ updateTask.py
@@ -4,7 +4,7 @@ import ast
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http import HTTPStatus
 from urllib.parse import urlparse, parse_qs
-from updateTasksInDB import updateTaskDay, createTask
+from updateTasksInDB import createTask, updateTaskName, updateTaskDay
 
 logging.basicConfig(level=logging.DEBUG)
 
