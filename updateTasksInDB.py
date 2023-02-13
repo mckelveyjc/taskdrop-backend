4282b02c8ea3648d80f871e69e2d904b5dd8212c
Turing <Turing@acad-jbws-10-4-66-93.stedwards.edu>
2023-02-13 11:32:58 -0600

    .

--- updateTasksInDB.py
+++ updateTasksInDB.py
@@ -20,11 +20,11 @@ def updateTaskDay(taskID, newDay):
             # cursor.execute("select * from tasks where taskUser=?", (1))
             # we'll do the below instead for now
             
-            # cursor.execute("update tasks set taskDay=%s where taskID=%s;" (newDay, taskID))
-            sql_update_query = """update tasks set taskDay = 'wednesday' where taskID = 2"""
+            cursor.execute("update tasks set taskDay=%s where taskID=%s;" (newDay, taskID))
+            # sql_update_query = """update tasks set taskDay = 'wednesday' where taskID = 2"""
             cursor.execute(sql_update_query)
             connection.commit()
-            
+
             # testing
             # cursor.execute("select * from tasks")
             
