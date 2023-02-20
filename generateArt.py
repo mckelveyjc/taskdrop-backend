a14c699fdfa24d056ec45f0d7777fbe8b74d2944
John C. Mckelvey <jmckelve@seu.stedwards.edu>
2023-02-19 23:59:50 -0600

    .

--- generateArt.py
+++ generateArt.py
@@ -1,47 +1,49 @@
-import os
-import openai
-import random
-from getTasksFromDB import getTasksForPrompt
+def printCat():
+    print("cat")
+# import os
+# import openai
+# import random
+# from getTasksFromDB import getTasksForPrompt
 
-# this function takes three random tasks from the "recently_completed" table and generates an 
-#   prompt for the AI art generator based on that prompt
-def createPrompt():
-    # first, let's get three random tasks from the database
-    # this is the specific task since they look like this:
-    # [('feed the dog',), ('eat veggies',), ('c',)]
-    # print(getTasksForPrompt()[0][0])
+# # this function takes three random tasks from the "recently_completed" table and generates an 
+# #   prompt for the AI art generator based on that prompt
+# def createPrompt():
+#     # first, let's get three random tasks from the database
+#     # this is the specific task since they look like this:
+#     # [('feed the dog',), ('eat veggies',), ('c',)]
+#     # print(getTasksForPrompt()[0][0])
 
-    prompt = ""
-    randomTasksAsTuples = getTasksForPrompt()
-    for taskIndex in range(len(randomTasksAsTuples)):
-        prompt += randomTasksAsTuples[taskIndex][0] + ", "
+#     prompt = ""
+#     randomTasksAsTuples = getTasksForPrompt()
+#     for taskIndex in range(len(randomTasksAsTuples)):
+#         prompt += randomTasksAsTuples[taskIndex][0] + ", "
 
-    randomArtStyleList = [
-        "picasso", 
-        "van gogh", 
-        "rembrandt", 
-        "cyber punk",
-        "medieval",
-        "wild west", 
-        "a 90s sitcom"
-        # any other crazy shit we can think of
-    ]
+#     randomArtStyleList = [
+#         "picasso", 
+#         "van gogh", 
+#         "rembrandt", 
+#         "cyber punk",
+#         "medieval",
+#         "wild west", 
+#         "a 90s sitcom"
+#         # any other crazy shit we can think of
+#     ]
 
-    prompt += "in the style of " + random.choice(randomArtStyleList)
-    return prompt
+#     prompt += "in the style of " + random.choice(randomArtStyleList)
+#     return prompt
 
-# this function takes the prompt and sends a request for an AI generated image to openAI
-def openAIArtRequest():
-    # openai.api_key = os.getenv("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")
-    generatedPrompt = createPrompt()
-    openai.api_key = "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"
-    generatedImageData = openai.Image.create(
-        prompt= generatedPrompt,
-        n=1,
-        size="1024x1024"
-    )
-    generatedImageUrl = generatedImageData["data"][0]["url"]
-    return generatedImageUrl
+# # this function takes the prompt and sends a request for an AI generated image to openAI
+# def openAIArtRequest():
+#     # openai.api_key = os.getenv("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")
+#     generatedPrompt = createPrompt()
+#     openai.api_key = "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"
+#     generatedImageData = openai.Image.create(
+#         prompt= generatedPrompt,
+#         n=1,
+#         size="1024x1024"
+#     )
+#     generatedImageUrl = generatedImageData["data"][0]["url"]
+#     return generatedImageUrl
 
-# print(openAIArtRequest())
-# print(createPrompt())
+# # print(openAIArtRequest())
+# # print(createPrompt())
--- updateTask.py
+++ updateTask.py
@@ -4,7 +4,7 @@ import ast
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http import HTTPStatus
 from urllib.parse import urlparse, parse_qs
-from generateArt import createPrompt
+from generateArt import printCat
 from updateTasksInDB import createTask, updateTaskName, updateTaskDay, completeTask, getNumCompletedTasks
 
 logging.basicConfig(level=logging.DEBUG)
