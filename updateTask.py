7d2049fc82506eb2c4b5abbe0db2c04c6d8a2b17
Turing <Turing@acad-jbws-10-4-66-93.stedwards.edu>
2023-02-25 02:00:06 -0600

    .

--- updateTask.py
+++ updateTask.py
@@ -4,7 +4,7 @@ import ast
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from http import HTTPStatus
 from urllib.parse import urlparse, parse_qs
-from generateArt import createPrompt, openAIArtRequest
+from generateArt import openAIArtRequest
 # from updateTasksInDB import createTask, updateTaskName, updateTaskDay, completeTask, getNumCompletedTasks, clearRecentlyCompletedTasks
 from updateTasksInDB import createTask, updateTaskName, updateTaskDay, completeTask, getNumCompletedTasks
 # from manageImages import saveBase64Image, cat
@@ -155,8 +155,7 @@ class BaseAppService(BaseHTTPRequestHandler):
             # responseBody['data'] = numCompletedTasks
             if (numCompletedTasks >= 5): # should be === 5 eventually
                 # create a piece of art based on tasks
-                # generatedImageBase64 = openAIArtRequest()
-                openAIArtRequest()
+                generatedImageBase64 = openAIArtRequest()
                 # generate save the art in the droplet
                 # saveBase64Image(response["taskID"], generateImageBase64)
                 # saveBase64Image("1", generatedImageBase64)
