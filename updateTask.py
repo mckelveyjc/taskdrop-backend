7219a692535bb354b183c977ec4260da747c7e95
Turing <Turing@acad-jbws-10-4-66-93.stedwards.edu>
2023-02-25 03:06:56 -0600

    .

--- updateTask.py
+++ updateTask.py
@@ -159,10 +159,10 @@ class BaseAppService(BaseHTTPRequestHandler):
                 generatedImageData = openAIArtRequest()
                 # responseBody['data'] = generatedImageData
                 generatedImageBase64 = generatedImageData["data"][0]["b64_json"]
-                responseBody['data'] = generatedImageBase64
+                # responseBody['data'] = generatedImageBase64
                 # generate save the art in the droplet
-                # saveBase64Image(response["taskID"], generateImageBase64)
-                responseBody['fun data'] = saveBase64Image("1", generatedImageBase64)
+                saveBase64Image(response["taskID"], generatedImageBase64)
+                # saveBase64Image("1", generatedImageBase64)
                 # responseBody['data'] = cat()
                 # clearRecentlyCompletedTasks() # do this when we're done testing
                 
