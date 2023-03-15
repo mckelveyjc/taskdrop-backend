812fda775f7070487256767dabb00b3111311fba
john <jmckelve@stedwards.edu>
2023-03-15 17:50:24 -0500

    fixed another typo in connectToDB.py

--- connectToDB.py
+++ connectToDB.py
@@ -1,23 +1,26 @@
-def excecuteQueryOnDB(query, values=None):
+def excecuteQueryOnDB(sqlQuery, values=None):
     try:
         connection = mysql.connector.connect(
         host='localhost',
         database='test_db',
         user='python',
         password='cosc4360')
-    if connection.is_connected():
-        db_Info = connection.get_server_info()
-        cursor = connection.cursor()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
+        
+        if connection.is_connected():
+            db_Info = connection.get_server_info()
+            cursor = connection.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
  
-        sqlQuery = 
-        values = (taskUserID,imageFileName)
-        cursor.execute(sqlInsertImgQuery, values)
-        connection.commit()
+            if values == None:
+                cursor.execute(sqlQuery)
+            else:
+                cursor.execute(sqlQuery, values)
+            
+            connection.commit()
 
-        data = cursor.fetchall()
-        return data
+            data = cursor.fetchall()
+            return data
     
     except Error as e:
         print("Error while connecting to MySQL", e)
